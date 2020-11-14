dbc77c4d04b9b532f6aaee74f3d634a594e91d2e
unknown <patpat12345@hotmail.com>
2020-11-14 00:22:52 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './components/pages/HomePage/Home'
+import Services from './components/pages/Services/Services'
+import Footer from './components/pages/Footer/Footer';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/services' component={Services}/>
+        <Route path='/products' component={Products}/>
+        <Route path='/sign-up' component={SignUp}/>
+      </Switch>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
